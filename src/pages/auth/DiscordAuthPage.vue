2a16d1d7b6193fc1575ad67031d930093762b0c9
Felix <jonfelixrico@gmail.com>
2022-10-20 00:20:36 +0800

    Integrate OAuth to the button

--- src/pages/auth/DiscordAuthPage.vue
+++ src/pages/auth/DiscordAuthPage.vue
@@ -1,9 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card>
+  <q-page class="flex flex-center bg-grey-2">
+    <q-card flat style="width: 500px; height: 250px" class="flex flex-center">
       <q-card-section>
-        <q-btn :label="$t('auth.loginWithDiscord')" />
+        <q-btn
+          :label="$t('auth.loginWithDiscord')"
+          :href="authUrl"
+          color="primary"
+          unelevated
+        />
       </q-card-section>
     </q-card>
   </q-page>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  computed: {
+    authUrl() {
+      return process.env.DISCORD_OAUTH_URL
+    },
+  },
+})
+</script>
