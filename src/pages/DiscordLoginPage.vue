6a1ff5b96ca2186ca5067f06279f9b384ba0e6d6
Felix <jonfelixrico@gmail.com>
2022-10-19 23:15:01 +0800

    Create placeholder code for the auth page

---
+++ src/pages/DiscordLoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-card-section>
+        <q-btn :label="$t('auth.loginWithDiscord')" />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
