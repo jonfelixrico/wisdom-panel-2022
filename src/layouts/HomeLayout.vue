09e7e0b83aec76d612fce4830c184d216ac190e8
Felix <jonfelixrico@gmail.com>
2022-10-22 03:30:15 +0800

    Integrate into HomeLayout

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -12,3 +12,14 @@
     </q-page-container>
   </q-layout>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useDiscordUserDataLoader } from 'src/composables/use-discord-user-data-loader.composable'
+
+export default defineComponent({
+  setup() {
+    useDiscordUserDataLoader()
+  },
+})
+</script>
