0e0904b3b640b75bd00812d111245d384af91738
Felix <jonfelixrico@gmail.com>
2022-10-22 03:42:42 +0800

    Create ResizeObserverContainer

---
+++ src/components/ResizeObserverContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- We want one more wrapping div to avoid any possible interference with this div -->
+    <div class="relative-position fit">
+      <q-resize-observer @resize="size = $event" />
+      <slot
+        class="absolute"
+        :style="{ width: `${size.width}px`, height: `${size.height}px` }"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const size = ref({
+      width: 0,
+      height: 0,
+    })
+
+    return {
+      size,
+    }
+  },
+})
+</script>
