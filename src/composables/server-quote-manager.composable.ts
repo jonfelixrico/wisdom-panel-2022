bcf0c979c49cccc3a194609229350fa8510d465f
Felix <jonfelixrico@gmail.com>
2022-10-22 19:00:04 +0800

    Create composable for fetching and reading the quotes under a server

---
+++ src/composables/server-quote-manager.composable.ts
@@ -0,0 +1,25 @@
+import { useQuotesStore } from 'src/stores/quotes-store'
+import { Quote } from 'src/types/quote.interface'
+import { computed } from 'vue'
+import { useApi } from './use-api.composable'
+
+export function useServerQuoteManager(serverId: string) {
+  const store = useQuotesStore()
+  const api = useApi()
+
+  async function load() {
+    const { data } = await api.get<Quote[]>(`server/${serverId}/quote`)
+    if (data) {
+      store.addQuotes(data)
+    }
+  }
+
+  const serverQuotes = computed(() =>
+    store.quotes?.filter((q) => q.serverId === serverId)
+  )
+
+  return {
+    load,
+    quotes: serverQuotes,
+  }
+}
