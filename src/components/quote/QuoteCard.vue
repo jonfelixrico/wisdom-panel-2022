b87993f53c3c5b080fc426620c7c44384b9f2c99
Felix <jonfelixrico@gmail.com>
2022-10-23 02:40:14 +0800

    Create QuoteCard component

---
+++ src/components/quote/QuoteCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card flat>
+    <q-card-section>
+      <template v-if="quote">
+        <div>{{ quote.content }}</div>
+        <i18n-t keypath="quote.authorFormat">
+          <template #author>
+            <span v-if="author !== undefined">{{ author }}</span>
+            <q-skeleton v-else />
+          </template>
+          <template #year>{{ quote.submitDt.getFullYear() }}</template>
+        </i18n-t>
+      </template>
+
+      <template v-else>
+        <q-skeleton type="rect" />
+        <q-skeleton type="text" />
+      </template>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Quote } from 'src/types/quote.interface'
+
+export default defineComponent({
+  props: {
+    quote: {
+      type: Object as PropType<Quote>,
+    },
+
+    author: String,
+    submitter: String,
+  },
+})
+</script>
