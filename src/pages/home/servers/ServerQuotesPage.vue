b85a87b7fc5bc4bd8508ebd0a35c3b8920704ded
Felix <jonfelixrico@gmail.com>
2022-10-22 21:09:51 +0800

    Remove the members loader for now

--- src/pages/home/servers/ServerQuotesPage.vue
+++ src/pages/home/servers/ServerQuotesPage.vue
@@ -9,7 +9,6 @@
 
 <script lang="ts">
 import { useServerMemberNameMap } from 'src/composables/server-member-name-map.composable'
-import { useServerMembersLoader } from 'src/composables/server-members-loader.composable'
 import { useServerQuoteManager } from 'src/composables/server-quote-manager.composable'
 import { defineComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
@@ -20,11 +19,10 @@ export default defineComponent({
     const serverId = route.params.serverId.toString()
 
     const quoteManager = useServerQuoteManager(serverId)
-    const membersLoader = useServerMembersLoader(serverId)
     const map = useServerMemberNameMap(serverId)
 
     onMounted(async () => {
-      await Promise.all([quoteManager.load(), membersLoader.load()])
+      await Promise.all([quoteManager.load()])
     })
 
     return {
