99538e6739e58e71f787e8b1a1b4c2edb8f6dca9
Felix <jonfelixrico@gmail.com>
2022-10-23 00:37:25 +0800

    Load the users

--- src/pages/home/servers/ServerQuotesPage.vue
+++ src/pages/home/servers/ServerQuotesPage.vue
@@ -9,6 +9,7 @@
 
 <script lang="ts">
 import { useServerMemberNameMap } from 'src/composables/server-member-name-map.composable'
+import { useServerMembersLoader } from 'src/composables/server-members-loader.composable'
 import { useServerQuoteManager } from 'src/composables/server-quote-manager.composable'
 import { defineComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
@@ -19,10 +20,11 @@ export default defineComponent({
     const serverId = route.params.serverId.toString()
 
     const quoteManager = useServerQuoteManager(serverId)
+    const membersLoader = useServerMembersLoader(serverId)
     const map = useServerMemberNameMap(serverId)
 
     onMounted(async () => {
-      await Promise.all([quoteManager.load()])
+      await Promise.all([quoteManager.load(), membersLoader.load()])
     })
 
     return {
