5bd81e69aaa8e54a4aa268766794634bb42a4f83
Felix <jonfelixrico@gmail.com>
2022-10-23 02:50:38 +0800

    Integrate into ServerQuotesPage

--- src/pages/home/servers/ServerQuotesPage.vue
+++ src/pages/home/servers/ServerQuotesPage.vue
@@ -1,9 +1,13 @@
 <template>
   <q-page class="column items-center">
-    <div v-for="quote of quotes" :key="quote.id">
-      {{ quote.content }}
-      {{ names[quote.authorId] }}
-    </div>
+    <QuoteCard
+      style="width: 500px"
+      v-for="quote of quotes"
+      :key="quote.id"
+      :quote="quote"
+      :author="names[quote.authorId]"
+      :submitter="names[quote.submitterId]"
+    />
   </q-page>
 </template>
 
@@ -13,24 +17,23 @@ import { useServerMembersLoader } from 'src/composables/server-members-loader.co
 import { useServerQuoteManager } from 'src/composables/server-quote-manager.composable'
 import { defineComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import QuoteCard from 'src/components/quote/QuoteCard.vue'
 
 export default defineComponent({
   setup() {
     const route = useRoute()
     const serverId = route.params.serverId.toString()
-
     const quoteManager = useServerQuoteManager(serverId)
     const membersLoader = useServerMembersLoader(serverId)
     const map = useServerMemberNameMap(serverId)
-
     onMounted(async () => {
       await Promise.all([quoteManager.load(), membersLoader.load()])
     })
-
     return {
       quotes: quoteManager.quotes,
       names: map,
     }
   },
+  components: { QuoteCard },
 })
 </script>
