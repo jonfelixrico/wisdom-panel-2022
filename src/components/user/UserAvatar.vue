66960453f60a0a489065765e58222277f4a19614
Felix <jonfelixrico@gmail.com>
2022-10-23 16:25:03 +0800

    Create UserAvatar component

---
+++ src/components/user/UserAvatar.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-avatar>
+    <q-img :src="avatarUrl" />
+  </q-avatar>
+</template>
+
+<script lang="ts">
+import {
+  APIUser,
+  CDNRoutes,
+  DefaultUserAvatarAssets,
+  ImageFormat,
+  RouteBases,
+} from 'discord-api-types/v10'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    user: {
+      type: Object as PropType<APIUser>,
+      required: true,
+    },
+  },
+
+  computed: {
+    avatarUrl() {
+      const { user } = this
+      let url: URL
+
+      if (user.avatar) {
+        url = new URL(
+          CDNRoutes.userAvatar(user.id, user.avatar, ImageFormat.WebP),
+          RouteBases.cdn
+        )
+      } else {
+        const avatarCode = (parseInt(user.discriminator) %
+          5) as DefaultUserAvatarAssets
+        url = new URL(CDNRoutes.defaultUserAvatar(avatarCode), RouteBases.cdn)
+      }
+
+      return url.toString()
+    },
+  },
+})
+</script>
