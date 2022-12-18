b0b0c04dd643fbf6ec1b98a7a6a71fce10e505a8
Felix <jonfelixrico@gmail.com>
2022-12-19 00:14:05 +0800

    Create beforeRouteEnter

--- src/pages/ReceiveQuickPreviewPage.vue
+++ src/pages/ReceiveQuickPreviewPage.vue
@@ -3,7 +3,39 @@
 </template>
 
 <script lang="ts">
+import { api } from 'src/boot/axios'
 import { defineComponent } from 'vue'
+import { useDiscordStore } from 'src/stores/discord-store'
+import { APIGuild } from 'discord-api-types/v10'
+import { getLogger } from 'src/boot/pino-logger'
 
-export default defineComponent({})
+export default defineComponent({
+  async beforeRouteEnter(to, from, next) {
+    const logger = getLogger('receive-quick-preview:beforeRouteEnter')
+
+    const { params } = to
+    const serverId = params.serverId as string
+    const receiveId = params.receiveId as string
+
+    const store = useDiscordStore()
+
+    let server = store.servers[serverId]
+    if (!server) {
+      try {
+        const { data } = await api.get<APIGuild>(`/server/${serverId}`)
+        server = data
+      } catch (e) {
+        // TODO only do next if 404 is detected
+        next(false)
+        return
+      }
+    } else if (server === 'NO_ACCESS') {
+      logger.warn(
+        `User has no acccess to server ${serverId}, aborting navigation`
+      )
+      next(false)
+      return
+    }
+  },
+})
 </script>
