f797dc5e3d52aecc82eb89a1942bc0370db6ad5e
Felix <jonfelixrico@gmail.com>
2022-12-18 23:18:12 +0800

    Adjust comments

--- src/boot/pino-logger.ts
+++ src/boot/pino-logger.ts
@@ -6,6 +6,13 @@ const pinoInstance = pino({
   level: 'debug', // TODO let the env vars dicate this
 })
 
+/**
+ * Statically imports the logger.
+ * Recommended to be used outside of components (e.g. utils, classes).
+ *
+ * @param context
+ * @returns
+ */
 export function getLogger(context?: string) {
   return pinoInstance.child({
     mode: 'static',
@@ -14,6 +21,13 @@ export function getLogger(context?: string) {
 }
 
 const LOGGER_TOKEN: InjectionKey<Logger> = Symbol('logger token')
+/**
+ * Inject the logger via Vue inject/provide.
+ * Recommended to be used inside components.
+ *
+ * @param context
+ * @returns
+ */
 export function useLogger(context?: string) {
   const logger = inject(LOGGER_TOKEN, pinoInstance)
   return logger.child({
