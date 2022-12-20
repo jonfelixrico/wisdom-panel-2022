0f277f2d179784a5232e2a06a4f68ab0d06c6775
Felix <jonfelixrico@gmail.com>
2022-12-21 00:34:21 +0800

    Integrate into the page

--- src/pages/preview/receive/ReceiveQuickPreviewPage.vue
+++ src/pages/preview/receive/ReceiveQuickPreviewPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center bg-deep-purple-6">
-    <CQuoteCard v-if="quote" :quote="quote" />
+    <CQuoteCard v-if="quote" :quote="quote" style="width: 50%" />
   </q-page>
 </template>
 
