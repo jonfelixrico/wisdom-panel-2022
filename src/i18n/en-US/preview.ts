b3965823ff9692fc409591124f7cd9d3ecac1bb8
Felix <jonfelixrico@gmail.com>
2022-12-20 22:06:27 +0800

    Create preview strings

---
+++ src/i18n/en-US/preview.ts
@@ -0,0 +1,9 @@
+export default {
+  errors: {
+    receiveEnter: {
+      title: 'Oops!',
+      serverNoAccess: 'You are not a member of this server.',
+      receiveNonExistent: "Can't seem to find the requested receive",
+    },
+  },
+}
