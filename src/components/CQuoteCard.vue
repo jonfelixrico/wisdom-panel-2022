3e3d2be01925b6fa22bbbe1e0035af046054df17
Felix <jonfelixrico@gmail.com>
2022-12-21 00:34:29 +0800

    Flesh out the component

--- src/components/CQuoteCard.vue
+++ src/components/CQuoteCard.vue
@@ -1,7 +1,29 @@
 <template>
   <q-card flat>
     <q-card-section>
-      {{ quote.content }}
+      <div class="q-gutter-y-sm">
+        <div class="text-h4 text-center text-weight-light">
+          "{{ quote.content }}"
+        </div>
+
+        <q-separator />
+
+        <!-- i18nize the format -->
+        <div class="row items-center">
+          <div class="col row justify-end">
+            <q-avatar>
+              <q-icon name="person" />
+            </q-avatar>
+          </div>
+          <div class="row justify-center">
+            <span class="text-h6 text-weight-regular">
+              {{ quote.authorId }}, {{ year }}
+            </span>
+          </div>
+
+          <div class="col" />
+        </div>
+      </div>
     </q-card-section>
   </q-card>
 </template>
@@ -15,6 +37,12 @@ export default defineComponent({
     quote: {
       required: true,
       type: Object as PropType<Quote>,
+    },
+  },
+
+  computed: {
+    year() {
+      return new Date(this.quote.submitDt).getFullYear()
     },
   },
 })
