4232735b3d407a9fb24451c22bc98d7410a24633
Felix <jonfelixrico@gmail.com>
2022-12-21 00:06:19 +0800

    Create display component

---
+++ src/components/receive-preview/CReceivePreview.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-card flat>
+    <q-card-section>
+      {{ quote.content }}
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { Quote } from 'src/models/quote.interface'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    quote: {
+      required: true,
+      type: Object as PropType<Quote>,
+    },
+  },
+})
+</script>
