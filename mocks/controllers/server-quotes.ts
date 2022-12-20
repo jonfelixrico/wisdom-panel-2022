ec3e5e195b824fcab323dcf5088db8a1a1229548
Felix <jonfelixrico@gmail.com>
2022-12-20 23:17:57 +0800

    Flesh out the details

--- mocks/controllers/server-quotes.ts
+++ mocks/controllers/server-quotes.ts
@@ -9,13 +9,41 @@ export function serverQuotesController(app: Router) {
 
   router.get('/dummy', (req, res) => {
     res.json({
-      quoteId: 'dummy',
+      id: 'dummy',
+      content: 'Dummy quote',
+
+      authorId: 'user-1',
+      submitterId: 'user-2',
+      submitDt: new Date('2022-01-01'),
+
+      receives: [
+        {
+          id: 'receive-1',
+          receiverId: 'user-3',
+          receiveDt: new Date('2022-01-02'),
+        },
+        {
+          id: 'receive-2',
+          receiverId: 'user-3',
+          receiveDt: new Date('2022-01-02'),
+        },
+        {
+          id: 'receive-3',
+          receiverId: 'user-3',
+          receiveDt: new Date('2022-01-02'),
+        },
+        {
+          id: 'receive-4',
+          receiverId: 'user-3',
+          receiveDt: new Date('2022-01-02'),
+        },
+      ],
     })
   })
 
   router.get('/:quoteId', (req, res) => {
     res.json({
-      quoteId: req.params.quoteId,
+      id: req.params.quoteId,
     })
   })
 
