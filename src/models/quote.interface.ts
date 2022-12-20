cde88289d65044240a176db633dff0653bd2c21e
Felix <jonfelixrico@gmail.com>
2022-12-20 23:33:25 +0800

    Create interface for quote

---
+++ src/models/quote.interface.ts
@@ -0,0 +1,16 @@
+export interface QuoteReceive {
+  id: string
+  receiverId: string
+  receiveDt: Date
+}
+
+export interface Quote {
+  id: string
+  content: string
+  authorId: string
+
+  submitterId: string
+  submitDt: Date
+
+  receives: QuoteReceive[]
+}
