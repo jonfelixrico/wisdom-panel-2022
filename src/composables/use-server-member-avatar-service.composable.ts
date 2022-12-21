4bb6df87ed348e7aa2dfcbb10f6f544491bc439d
Felix <jonfelixrico@gmail.com>
2022-12-21 22:16:13 +0800

    Create composable for avatar URL

---
+++ src/composables/use-server-member-avatar-service.composable.ts
@@ -0,0 +1,32 @@
+import { useAvatarStore } from 'src/stores/avatar-store'
+import { useApi } from './use-api.composable'
+
+export function useServerMemberAvatarService() {
+  const store = useAvatarStore()
+  const api = useApi()
+
+  /**
+   * Retrieves the avatar URL from the server.
+   * Caching is implemented.
+   */
+  async function getAvatarUrl(userId: string, serverId: string) {
+    let url = store.servers[serverId]?.userId
+    if (!url) {
+      const { data } = await api.get<{ url: string }>(
+        `server/${serverId}/user/${userId}/avatar`
+      )
+
+      url = data.url
+      store.setServerMemberAvatarUrl(userId, serverId, url)
+    }
+
+    url
+  }
+
+  return {
+    /**
+     * Fetches the URL to the server member's avatar.
+     */
+    getAvatarUrl,
+  }
+}
