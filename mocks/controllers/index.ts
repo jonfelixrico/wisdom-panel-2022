a49e1828deec369f68a26342677e087f0123c0e3
Felix <jonfelixrico@gmail.com>
2022-12-21 22:40:31 +0800

    Integrate into the controllers

--- mocks/controllers/index.ts
+++ mocks/controllers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { serverMembersController } from './server-members.controller'
 import { serverQuotesController } from './server-quotes.controller'
 import { serversController } from './server.controller'
 
 export function controllers(app: Router) {
   serversController(app)
   serverQuotesController(app)
+  serverMembersController(app)
 }
