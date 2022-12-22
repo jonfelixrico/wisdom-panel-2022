68d72c47d3249d94c1a8817f39a8851d0f4f9bdf
Felix <jonfelixrico@gmail.com>
2022-12-22 22:37:27 +0800

    Create initial code of CServerMemberChip

---
+++ src/components/server-member/CServerMemberChip.vue
@@ -0,0 +1,24 @@
+<template>
+  <CServerMemberAvatar :user="user" />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import CServerMemberAvatar from './CServerMemberAvatar.vue'
+
+interface ServerMember {
+  userId: string
+  serverId: string
+}
+
+export default defineComponent({
+  components: { CServerMemberAvatar },
+
+  props: {
+    user: {
+      type: Object as PropType<ServerMember>,
+      required: true,
+    },
+  },
+})
+</script>
