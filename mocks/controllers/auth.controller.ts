77c3b59a72cbbd78caf3d62b2e2a44973800b43b
Felix <jonfelixrico@gmail.com>
2022-12-27 19:18:08 +0800

    Create mock code for the oauth process

---
+++ mocks/controllers/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import { URL } from 'url'
+
+export function authController(app: Router) {
+  const router = Router()
+
+  router.get('/auth/oauth/discord', (req, res) => {
+    const redirectUrl = new URL(
+      'http://localhost:8080/auth/oauth/discord/callback'
+    )
+
+    const query = redirectUrl.searchParams
+    query.append('code', 'dummy')
+
+    res.redirect(redirectUrl.toString())
+  })
+
+  router.post('/auth/oauth/discord', (req, res) => {
+    res.end()
+  })
+
+  app.use('/auth', router)
+}
