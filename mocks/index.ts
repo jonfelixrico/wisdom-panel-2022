456b8ff23ccf7ee322b64048439204999cb26351
Felix <jonfelixrico@gmail.com>
2022-12-29 16:32:40 +0800

    Add comments

--- mocks/index.ts
+++ mocks/index.ts
@@ -5,10 +5,14 @@ import { setupSession } from './setup/session.setup'
 
 const app = express()
 
+/*
+ * - prevents loading of favicon (we dont have that)
+ * - must be declared before setupSession to avoid 401s for favicon calls (we'll still have them)
+ */
+app.get('/favicon.ico', (req, res) => res.sendStatus(204))
+
 setupSession(app)
 setupHandlebars(app)
-
-app.get('/favicon.ico', (req, res) => res.sendStatus(204))
 
 controllers(app)
 
