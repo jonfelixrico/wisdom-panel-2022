58646cba0db4fcade09ea31f39df2d3d4b0b886a
Felix <jonfelixrico@gmail.com>
2022-12-29 15:05:27 +0800

    Create route guard for session-checking

---
+++ src/route-guards/session-check.guard.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { api } from 'src/boot/axios'
+import { getLogger } from 'src/boot/pino-logger'
+import { useSessionStore } from 'src/stores/session-store'
+import { NavigationGuard } from 'vue-router'
+
+const LOGGER = getLogger('auth-guard')
+
+export const checkSession: NavigationGuard = async (to, from, next) => {
+  const sessionStore = useSessionStore()
+  if (!sessionStore.hasSession) {
+    try {
+      await api.head('session')
+      sessionStore.setHasSession(true)
+      LOGGER.info('Session found.')
+    } catch (e) {
+      if (!axios.isAxiosError(e)) {
+        LOGGER.error(e, 'Error encountered while trying to check for session')
+      } else if (e.response?.status !== 401) {
+        LOGGER.error(e, 'Non-401 error encountered while doing a session check')
+      } else {
+        LOGGER.warn('Encountered 401 while doing a session check')
+      }
+
+      next({ name: 'index' })
+    }
+  }
+}
