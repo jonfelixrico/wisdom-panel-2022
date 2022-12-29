249bd5ab9df34e2f5d0ac062d5ce1331a4faee40
Felix <jonfelixrico@gmail.com>
2022-12-29 15:31:34 +0800

    Adjust the session guard

--- src/route-guards/session-check.guard.ts
+++ src/route-guards/session-check.guard.ts
@@ -8,21 +8,27 @@ const LOGGER = getLogger('auth-guard')
 
 export const checkSession: NavigationGuard = async (to, from, next) => {
   const sessionStore = useSessionStore()
-  if (!sessionStore.hasSession) {
-    try {
-      await api.head('session')
-      sessionStore.setHasSession(true)
-      LOGGER.info('Session found.')
-    } catch (e) {
-      if (!isAxiosError(e)) {
-        LOGGER.error(e, 'Error encountered while trying to check for session')
-      } else if (e.response?.status !== 401) {
-        LOGGER.error(e, 'Non-401 error encountered while doing a session check')
-      } else {
-        LOGGER.warn('Encountered 401 while doing a session check')
-      }
 
-      next({ name: 'index' })
+  if (to.meta.isPublicRoute || sessionStore.hasSession) {
+    next()
+    return
+  }
+
+  try {
+    await api.head('session')
+    LOGGER.info('Session found.')
+
+    sessionStore.setHasSession(true)
+    next()
+  } catch (e) {
+    if (!isAxiosError(e)) {
+      LOGGER.error(e, 'Error encountered while trying to check for session')
+    } else if (e.response?.status !== 401) {
+      LOGGER.error(e, 'Non-401 error encountered while doing a session check')
+    } else {
+      LOGGER.warn('Encountered 401 while doing a session check')
     }
+
+    next({ name: 'index' })
   }
 }
