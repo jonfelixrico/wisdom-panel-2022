953ad4433d20aacb3b5d88cb754ef037b2b28251
Felix <jonfelixrico@gmail.com>
2022-12-29 14:40:49 +0800

    Move out the handlebars setup

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,19 +1,14 @@
 import express from 'express'
 import { controllers } from './controllers'
-import { engine } from 'express-handlebars'
-import path from 'path'
+import { setupHandlebars } from './setup/handlebars.setup'
 import { setupSession } from './setup/session.setup'
 
 const app = express()
 
 controllers(app)
 
-// Handlebars setup
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, 'views'))
-
 setupSession(app)
+setupHandlebars(app)
 
 app.listen(9081)
 
