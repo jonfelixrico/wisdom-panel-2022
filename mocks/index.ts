84ae07817f50fbd6e8145155092999c88308e091
Felix <jonfelixrico@gmail.com>
2022-12-29 12:36:03 +0800

    Set up handlebars

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import { controllers } from './controllers'
+import { engine } from 'express-handlebars'
+import path from 'path'
 
 const app = express()
 
@@ -8,6 +10,11 @@ app.get('/', (req, res) => {
 })
 
 controllers(app)
+
+// Handlebars setup
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
 
 app.listen(9081)
 
