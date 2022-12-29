11829d3681074c8191fac59379989a7de1378878
Felix <jonfelixrico@gmail.com>
2022-12-29 15:37:25 +0800

    Create fetchSession method

--- src/stores/session-store.ts
+++ src/stores/session-store.ts
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
+import { isAxiosError } from 'src/utils/axios.utils'
 
 export const useSessionStore = defineStore('session', {
   state: () => ({
@@ -8,6 +10,25 @@ export const useSessionStore = defineStore('session', {
   actions: {
     setHasSession(value: boolean) {
       this.hasSession = value
+    },
+
+    async fetchSession() {
+      if (this.hasSession) {
+        return true
+      }
+
+      try {
+        await api.head('session')
+        this.setHasSession(true)
+        return true
+      } catch (e) {
+        if (isAxiosError(e) && e.response?.status === 401) {
+          return false
+        }
+
+        // unexpected error, has to be handled by the caller
+        throw e
+      }
     },
   },
 })
