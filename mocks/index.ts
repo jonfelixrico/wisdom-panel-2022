bb3a1adbfa56f30a0f39c2103f2a951315f14e98
Felix <jonfelixrico@gmail.com>
2022-12-29 13:09:58 +0800

    Integrate sessions

--- mocks/index.ts
+++ mocks/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { controllers } from './controllers'
 import { engine } from 'express-handlebars'
 import path from 'path'
+import session from 'express-session'
 
 const app = express()
 
@@ -15,6 +16,15 @@ controllers(app)
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', path.join(__dirname, 'views'))
+
+// Session setup
+app.use(
+  session({
+    secret: 'mocks',
+    saveUninitialized: true,
+    resave: false,
+  })
+)
 
 app.listen(9081)
 
