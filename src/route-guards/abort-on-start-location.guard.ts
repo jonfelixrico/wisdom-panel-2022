b3f4378005d67ab4439b7fb22c1dfe28185cc381
Felix <jonfelixrico@gmail.com>
2022-12-29 23:13:44 +0800

    Adjust the abort on start guard

--- src/route-guards/abort-on-start-location.guard.ts
+++ src/route-guards/abort-on-start-location.guard.ts
@@ -7,8 +7,15 @@ import {
   Router,
 } from 'vue-router'
 
-const LOGGER = getLogger('guard:abort-on-start')
+/*
+ * This entire thing is to prevent the user from being stuck in a white-screen only page
+ * if the navigation was aborted during the starting route for the tab.
+ *
+ * If the user went to /route-1 and the `beforeRouteEnter` of the page yielded `next(false)`, if we did not
+ * have this handling, the user will only be shown a white screen.
+ */
 
+const LOGGER = getLogger('guard:abort-on-start')
 export const abortOnStartLocationGuard: (
   router: Router
 ) => NavigationHookAfter = (router) => {
@@ -22,9 +29,9 @@ export const abortOnStartLocationGuard: (
       return
     }
 
-    LOGGER.warn('Abort on START_LOCATION detected; pushing to home')
+    LOGGER.warn('Abort on START_LOCATION detected; pushing to index')
     router.push({
-      name: 'home',
+      name: 'index',
     })
   }
 }
