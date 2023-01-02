251b1037cf029d8a82236138f4397d0158d49e08
Felix <jonfelixrico@gmail.com>
2023-01-02 12:46:03 +0800

    Remove unused mock endpoints

--- mocks/controllers/server-member.controller.ts
+++ mocks/controllers/server-member.controller.ts
@@ -9,28 +9,6 @@ const usernameMap: Record<string, string> = {
 export function serverMemberController(app: Router) {
   const router = Router()
 
-  /**
-   * @deprecated
-   */
-  router.get('/:userId/avatar', (req, res) => {
-    res.json({
-      url: '/api/dummy-avatar',
-    })
-  })
-
-  /**
-   * @deprecated
-   */
-  router.get('/:userId/username', (req, res) => {
-    const username = usernameMap[req.params.userId]
-    if (!username) {
-      res.sendStatus(404)
-      return
-    }
-
-    res.json({ username })
-  })
-
   router.get('/:userId', (req, res) => {
     const username = usernameMap[req.params.userId]
     res.json({
