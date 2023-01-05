5a6c5ddea38a3fd99581064fd81a57515b30687a
Felix <jonfelixrico@gmail.com>
2023-01-06 00:32:32 +0800

    Add comments to the login flow

--- test/cypress/e2e-tests/login-flow.cy.ts
+++ test/cypress/e2e-tests/login-flow.cy.ts
@@ -1,25 +1,33 @@
 describe('Login flow', () => {
   it('should be able to handle the login happy path', () => {
+    // 401 will make the user stay on /login because they are unauthenticated
     cy.intercept('HEAD', '/api/session', {
       statusCode: 401,
     })
-
     cy.visit('/login')
 
     cy.intercept('/api/auth/oauth/discord', {
       fixture: 'html/login/success-oauth.html',
     }).as('redirectToOAuth')
 
+    // on click of the discord login button, they will be redirected to the OAuth page
     cy.dataCy('discord-login-btn').click()
     cy.wait('@redirectToOAuth')
 
+    /*
+     * by clicking this, user finishes OAuth process
+     *
+     * the user will actually be redirected to the callback page
+     */
+    cy.dataCy('redirect-href').click()
+
+    // this is to trick the callback page that the user really was successful
     cy.intercept('HEAD', '/api/session', {
       statusCode: 200,
     })
 
-    cy.dataCy('redirect-href').click()
-    cy.wait(500)
-
+    // callback page will redirect user to the landing page
+    cy.wait(200)
     cy.url().should('contain', 'landing')
   })
 })
