1e3b4594185505558b7213bde25901e2b696e7a9
Felix <jonfelixrico@gmail.com>
2023-01-05 23:33:40 +0800

    Push tests for login flow

--- test/cypress/e2e-tests/login-flow.cy.ts
+++ test/cypress/e2e-tests/login-flow.cy.ts
@@ -1,5 +1,5 @@
 describe('Login flow', () => {
-  it('should visit login page if index page was accessed', () => {
+  it('can pass the happy path', () => {
     cy.intercept('HEAD', '/api/session', {
       statusCode: 401,
     })
@@ -7,6 +7,22 @@ describe('Login flow', () => {
     cy.visit('/')
     cy.wait(500)
     cy.url().should('contain', 'login')
+
+    cy.intercept('/api/auth/oauth/discord', {
+      fixture: 'html/login/success-oauth.html',
+    }).as('redirectToOAuth')
+
+    cy.dataCy('discord-login-btn').click()
+    cy.wait('@redirectToOAuth')
+
+    cy.intercept('HEAD', '/api/session', {
+      statusCode: 200,
+    })
+
+    cy.dataCy('redirect-href').click()
+    cy.wait(500)
+
+    cy.url().should('contain', 'landing')
   })
 })
 
