8bbbbeaa9c5ff52db2973a2fde2d74b6886aa144
Felix <jonfelixrico@gmail.com>
2023-01-05 23:37:37 +0800

    Adjust login flow test case

--- test/cypress/e2e-tests/login-flow.cy.ts
+++ test/cypress/e2e-tests/login-flow.cy.ts
@@ -1,13 +1,10 @@
 describe('Login flow', () => {
-  // Login -- happy path
-  it('should guide the unauthenticated user through the flow', () => {
+  it('should be able to handle the login happy path', () => {
     cy.intercept('HEAD', '/api/session', {
       statusCode: 401,
     })
 
-    cy.visit('/')
-    cy.wait(500)
-    cy.url().should('contain', 'login')
+    cy.visit('/login')
 
     cy.intercept('/api/auth/oauth/discord', {
       fixture: 'html/login/success-oauth.html',
