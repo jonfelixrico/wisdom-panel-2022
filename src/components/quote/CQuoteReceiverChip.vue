8e8c69343d7b3da13d6400d720129dd976e13dd1
Felix <jonfelixrico@gmail.com>
2023-01-20 00:20:26 +0800

    Create QuoteReceiverChip

---
+++ src/components/quote/CQuoteReceiverChip.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-body rounded-borders q-px-xs row items-center">
+    <CServerMemberAvatar :user="user" size="xs" class="q-mr-xs" />
+    <CServerMemberName :user="user" />
+    <span class="pre text-grey-5 text-weight-medium"> x {{ count }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import CServerMemberAvatar from '../server-member/CServerMemberAvatar.vue'
+import CServerMemberName from '../server-member/CServerMemberName.vue'
+import { ServerMember } from '../server-member/server-member.interface'
+
+export default defineComponent({
+  components: { CServerMemberAvatar, CServerMemberName },
+
+  props: {
+    user: {
+      type: Object as PropType<ServerMember>,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+  },
+})
+</script>
