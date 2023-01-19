cf6f1c11cb0f8b7cf5376930d006c0d704048111
Felix <jonfelixrico@gmail.com>
2023-01-19 23:11:05 +0800

    Create the PromiseCache tool

---
+++ src/utils/promise-cache.utils.ts
@@ -0,0 +1,13 @@
+export class PromiseCache {
+  private cache: Record<string, Promise<unknown>> = {}
+
+  wrap<T>(jobId: string, fnToWrap: () => Promise<T>): Promise<T> {
+    if (!!this.cache[jobId]) {
+      return this.cache[jobId] as Promise<T>
+    }
+
+    const fnPromise = fnToWrap()
+    this.cache[jobId] = fnPromise
+    return fnPromise
+  }
+}
