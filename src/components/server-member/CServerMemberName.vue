85b715fe8bd298703c8924687dd9a87b72fa9e04
Felix <jonfelixrico@gmail.com>
2023-01-19 23:27:40 +0800

    Create component for lazy-loading and displaying server members

---
+++ src/components/server-member/CServerMemberName.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="userData" v-bind="$attrs" v-text="userData.username" />
+  <q-skeleton
+    v-else
+    type="text"
+    :width="`${skeletonWidth}px`"
+    :height="`${skeletonHeight}px`"
+  />
+</template>
+
+<script lang="ts">
+import { userServerMemberStore } from 'src/stores/server-member-store'
+import { computed, defineComponent, onBeforeMount, PropType } from 'vue'
+import { ServerMember } from './server-member.interface'
+
+export default defineComponent({
+  /*
+   * We're disabling attr inheritance to make parent-level class or style attributes
+   * not apply to the skeleton. We want them to apply in the username div only.
+   */
+  inheritAttrs: false,
+
+  props: {
+    user: {
+      type: Object as PropType<ServerMember>,
+      required: true,
+    },
+
+    skeletonWidth: {
+      type: Number,
+      default: 50,
+    },
+
+    skeletonHeight: {
+      type: Number,
+      default: 16,
+    },
+  },
+
+  setup(props) {
+    const store = userServerMemberStore()
+
+    onBeforeMount(() => {
+      store.fetchServerMember(props.user.serverId, props.user.userId)
+    })
+
+    return {
+      userData: computed(
+        () => store.servers[props.user.serverId]?.[props.user.userId]
+      ),
+    }
+  },
+})
+</script>
