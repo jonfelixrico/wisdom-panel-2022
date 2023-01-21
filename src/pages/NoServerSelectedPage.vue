238cc6dbf347ba418d8a3ff09790ad3fe68064df
Felix <jonfelixrico@gmail.com>
2023-01-22 00:27:57 +0800

    Apply minor changes to the no server selected page

--- src/pages/NoServerSelectedPage.vue
+++ src/pages/NoServerSelectedPage.vue
@@ -1,7 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="column items-center justify-center">
     <!-- TODO show a spinner instead if the list is still being loaded -->
-    <div class="text-h4">{{ $t('server.noServerSelected') }}</div>
+    <!-- TODO i18nize this -->
+    <div class="text-h2">Welcome!</div>
+    <div class="text-h4">
+      {{ $t('server.noServerSelected') }}
+    </div>
     <!-- TODO ask the user to add them to one of their servers if no servers are detected -->
   </q-page>
 </template>
