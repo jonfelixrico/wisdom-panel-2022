0faa3773580362c594dafb076dbdb2e704dd52d2
Felix <jonfelixrico@gmail.com>
2023-01-22 00:17:36 +0800

    Populate CServerMenu

--- src/components/server/CServerMenu.vue
+++ src/components/server/CServerMenu.vue
@@ -1,3 +1,45 @@
 <template>
-  <q-scroll-area> menu </q-scroll-area>
+  <q-scroll-area>
+    <q-list>
+      <CServerMenuItem
+        v-for="item of ITEMS"
+        :key="item.labelKey"
+        :label="$t(item.labelKey)"
+        @click="$router.push(item.location)"
+        :active="item.moduleKeys.includes(String($route.meta.moduleKey))"
+      />
+    </q-list>
+  </q-scroll-area>
 </template>
+
+<script lang="ts">
+import { RouteLocationRaw } from 'vue-router'
+import { defineComponent } from 'vue'
+import CServerMenuItem from './CServerMenuItem.vue'
+
+interface MenuItem {
+  labelKey: string
+  location: Partial<RouteLocationRaw>
+  moduleKeys: string[]
+}
+
+const ITEMS: MenuItem[] = [
+  {
+    labelKey: 'server.menuItems.quoteList',
+    location: {
+      name: 'quote-list',
+    },
+    moduleKeys: ['quote-list'],
+  },
+]
+
+export default defineComponent({
+  components: { CServerMenuItem },
+
+  setup() {
+    return {
+      ITEMS,
+    }
+  },
+})
+</script>
