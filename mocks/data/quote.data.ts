b6d761fa60be3ff590cba4f78d2717737817f10d
Felix <jonfelixrico@gmail.com>
2023-01-22 01:13:40 +0800

    Create methods for mocking quote list data

---
+++ mocks/data/quote.data.ts
@@ -0,0 +1,25 @@
+import { Quote } from 'src/models/quote.interface'
+
+export function generatePartialQuote(count: number) {
+  const partials: { quoteId: string }[] = []
+
+  for (let i = 1; i <= count; i++) {
+    partials.push({
+      quoteId: `quote-${i}`,
+    })
+  }
+
+  return partials
+}
+
+export function generateQuote(serverId: string, quoteId: string): Quote {
+  return {
+    id: quoteId,
+    serverId,
+    authorId: 'user-1',
+    submitterId: 'user-2',
+    content: `Fodder -- id ${quoteId} of server ${serverId}`,
+    receives: [],
+    submitDt: new Date('2022-01-01'),
+  }
+}
