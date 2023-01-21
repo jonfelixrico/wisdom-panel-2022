987b28474c34442cecc5c640aa5fdd76d3bc726f
Felix <jonfelixrico@gmail.com>
2023-01-22 00:54:27 +0800

    Create the card component for quotes

---
+++ src/components/quote/CQuoteCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card flat>
+    <q-card-section> aa </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    quoteId: {
+      type: String,
+      required: true,
+    },
+
+    serverId: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
