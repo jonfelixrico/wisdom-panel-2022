4a1deb529f9003e2653e87fce3263158a50b7111
Felix <jonfelixrico@gmail.com>
2023-01-21 23:33:46 +0800

    Add TODO

--- src/pages/NoServerSelectedPage.vue
+++ src/pages/NoServerSelectedPage.vue
@@ -1,5 +1,7 @@
 <template>
   <q-page class="flex flex-center">
+    <!-- TODO show a spinner instead if the list is still being loaded -->
     <div class="text-h4">{{ $t('server.noServerSelected') }}</div>
+    <!-- TODO ask the user to add them to one of their servers if no servers are detected -->
   </q-page>
 </template>
