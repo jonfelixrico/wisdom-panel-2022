364314447db19ec3675c230eea66836f890828d6
Felix <jonfelixrico@gmail.com>
2023-01-21 19:49:34 +0800

    Add more elements to MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,27 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-header> test </q-toolbar-header>
+      </q-toolbar>
+    </q-header>
+    <q-drawer v-model="showDrawer"> test </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const showDrawer = ref(true)
+
+    return {
+      showDrawer,
+    }
+  },
+})
+</script>
