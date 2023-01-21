661e806c49e20ee584f18095bd695d0049ee81ce
Felix <jonfelixrico@gmail.com>
2023-01-22 01:04:00 +0800

    Implement quote-loading functionality

--- src/pages/ServerQuotesPage.vue
+++ src/pages/ServerQuotesPage.vue
@@ -4,15 +4,51 @@
     <div class="col relative-position">
       <q-scroll-area class="absolute-full">
         <div class="q-mx-auto q-pa-sm content-max-width q-gutter-y-sm">
-          <q-card flat>
-            <q-card-section> aa </q-card-section>
-          </q-card>
+          <CQuoteCard
+            v-for="quote of quotes"
+            :key="quote.quoteId"
+            :quote-id="quote.quoteId"
+            :server-id="serverId"
+          />
 
-          <q-card flat>
-            <q-card-section> aa </q-card-section>
-          </q-card>
+          <!-- TODO skeletons while there are no quotes loaded -->
         </div>
       </q-scroll-area>
     </div>
   </q-page>
 </template>
+
+<script lang="ts">
+import { useRoute } from 'vue-router'
+import { useApi } from 'src/composables/use-api.composable'
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import CQuoteCard from 'src/components/quote/CQuoteCard.vue'
+
+interface PartialQuote {
+  quoteId: string
+}
+
+export default defineComponent({
+  // TODO implement cursor-based pagination
+  setup() {
+    const api = useApi()
+    const $route = useRoute()
+    const serverId = computed(() => String($route.params.serverId))
+
+    async function fetchQuotes(serverId: string) {
+      const { data } = await api.get<PartialQuote[]>(`server/${serverId}/quote`)
+      return data
+    }
+    const quotes = ref<PartialQuote[]>([])
+    onMounted(async () => {
+      quotes.value = await fetchQuotes(serverId.value)
+    })
+    return {
+      quotes,
+      serverId,
+    }
+  },
+
+  components: { CQuoteCard },
+})
+</script>
