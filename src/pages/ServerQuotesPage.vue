db685f48c310f4834049a9c11fbcb3bd647d87e1
Felix <jonfelixrico@gmail.com>
2023-01-22 00:45:18 +0800

    Add dummy content to ServerQuotesPage

--- src/pages/ServerQuotesPage.vue
+++ src/pages/ServerQuotesPage.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page class="column">
     <q-toolbar class="shadow-1" />
-    <q-scroll-area class="col"></q-scroll-area>
+    <div class="col relative-position">
+      <q-scroll-area class="absolute-full">
+        <div class="q-mx-auto q-pa-sm content-max-width q-gutter-y-sm">
+          <q-card flat>
+            <q-card-section> aa </q-card-section>
+          </q-card>
+
+          <q-card flat>
+            <q-card-section> aa </q-card-section>
+          </q-card>
+        </div>
+      </q-scroll-area>
+    </div>
   </q-page>
 </template>
