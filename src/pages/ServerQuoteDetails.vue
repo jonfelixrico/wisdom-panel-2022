0d20b043a710729b0ddab0badbe99c10df1a2abc
Felix <jonfelixrico@gmail.com>
2023-01-22 20:50:17 +0800

    Remove outdated comments

--- src/pages/ServerQuoteDetails.vue
+++ src/pages/ServerQuoteDetails.vue
@@ -2,7 +2,6 @@
   <q-page class="column">
     <q-toolbar class="shadow-1">
       <q-btn icon="arrow_back" flat round dense />
-      <!-- TODO i18nize -->
       <q-toolbar-title>{{ $t('quote.detailsPage.title') }}</q-toolbar-title>
     </q-toolbar>
     <div class="col"></div>
