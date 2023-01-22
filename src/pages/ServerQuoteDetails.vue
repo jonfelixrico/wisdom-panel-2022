ee502b924fb59ecb0f5c149f4008e99d4abf9934
Felix <jonfelixrico@gmail.com>
2023-01-22 15:49:45 +0800

    Create ServerQuoteDetails page

---
+++ src/pages/ServerQuoteDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="column">
+    <q-toolbar class="shadow-1" />
+    <div class="col"></div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
