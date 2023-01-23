5907bae405e49ef92e843368dc1f43a204ba0630
Felix <jonfelixrico@gmail.com>
2023-01-24 00:23:39 +0800

    Create the types

---
+++ src/types/core-api/core-api-quote.interface.ts
@@ -0,0 +1,43 @@
+export interface CoreAPIQuote {
+  id: string
+  content: string
+  authorId: string
+  submitterId: Date
+  submitDt: Date
+
+  serverId: string
+  channelId: string
+  messageId: string
+
+  receives: CoreAPIQuoteReceive[]
+
+  statusDeclaration?: CoreAPIQuoteStatusDeclaration
+
+  votes: Record<string, Date>
+  requiredVoteCount: number
+  /**
+   * Voting deadline.
+   */
+  expirationDt: Date
+
+  isLegacy: boolean
+  revision: number
+}
+
+export interface CoreAPIQuoteReceive {
+  id: string
+  timestamp: Date
+  userId: string
+
+  serverId: string
+
+  channelId?: string
+  messageId?: string
+
+  isLegacy: boolean
+}
+
+export interface CoreAPIQuoteStatusDeclaration {
+  status: 'EXPIRED' | 'APPROVED'
+  timestamp: Date
+}
