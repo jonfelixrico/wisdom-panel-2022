7bb9e79ac7550c8b628985a8e0d3762c84f92d5c
Felix <jonfelixrico@gmail.com>
2023-01-24 00:30:18 +0800

    Create the variants

--- src/types/quote.interface.ts
+++ src/types/quote.interface.ts
@@ -1,7 +1,30 @@
-interface QuoteBase {
+/**
+ * Base interface for quotes.
+ */
+export interface BaseQuote {
   id: string
   content: string
   authorId: string
   submitterId: Date
   submitDt: Date
+  serverId: string
+  type: 'APPROVED' | 'PENDING'
+}
+
+export interface ApprovedQuote extends BaseQuote {
+  type: 'APPROVED'
+
+  receives: {
+    id: string
+    timestamp: Date
+    userId: string
+  }[]
+}
+
+export interface PendingQuote extends BaseQuote {
+  type: 'PENDING'
+
+  expirationDt: Date
+  votes: Record<string, Date>
+  requiredVoteCount: number
 }
