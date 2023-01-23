d9df1f70ec470a513f2e3c27bf9617cdae974f0c
Felix <jonfelixrico@gmail.com>
2023-01-24 01:48:55 +0800

    Remove the dummy

--- mocks/controllers/server-quotes.controller.ts
+++ mocks/controllers/server-quotes.controller.ts
@@ -1,21 +1,5 @@
 import { Router } from 'express'
-import { Quote } from 'src/stores/quote-store'
 import { generateFodderQuote, generateQuoteListData } from '../data/quote.data'
-
-function generateReceives() {
-  const RECEIVE_COUNT = 30
-  const UNIQUE_USER_COUNT = 10
-  const receives: { id: string; userId: string }[] = []
-
-  for (let receiveNo = 1; receiveNo <= RECEIVE_COUNT; receiveNo++) {
-    receives.push({
-      id: `receive-${receiveNo}`,
-      userId: `user-${receiveNo % UNIQUE_USER_COUNT || UNIQUE_USER_COUNT}`,
-    })
-  }
-
-  return receives
-}
 
 const QUOTE_LIST_DATA = generateQuoteListData(100)
 
@@ -46,23 +30,6 @@ export function serverQuotesController(app: Router) {
         })
       )
     }
-  })
-
-  router.get('/server/:serverId/quote/dummy', (req, res) => {
-    res.json({
-      id: 'dummy',
-      content:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-
-      authorId: 'user-1',
-      submitterId: 'user-2',
-
-      serverId: req.params.serverId,
-
-      receives: generateReceives(),
-      status: 'ACCEPTED',
-      submitDt: new Date('2022-01-01'),
-    } as Quote)
   })
 
   router.get('/server/:serverId/quote/quote-fodder-:seq', (req, res) => {
