ba15ba87673f7ec6a326cb4b954f368909322c21
Felix <jonfelixrico@gmail.com>
2023-01-24 00:53:07 +0800

    Create consumer util

---
+++ src/utils/core-api-quote.utils.ts
@@ -0,0 +1,76 @@
+import { plainToInstance, Type } from 'class-transformer'
+import {
+  CoreAPIQuote,
+  CoreAPIQuoteReceive,
+  CoreAPIQuoteStatusDeclaration,
+} from 'src/types/core-api/core-api-quote.interface'
+import { ApprovedQuote, PendingQuote } from 'src/types/quote.interface'
+
+class CoreAPIQuoteReceiveTransformer implements CoreAPIQuoteReceive {
+  id!: string
+
+  @Type(() => Date)
+  timestamp!: Date
+
+  userId!: string
+  serverId!: string
+  channelId?: string
+  messageId?: string
+  isLegacy!: boolean
+}
+
+class CoreAPIQuoteStatusDeclarationTransformer
+  implements CoreAPIQuoteStatusDeclaration
+{
+  status!: 'EXPIRED' | 'APPROVED'
+
+  @Type(() => Date)
+  timestamp!: Date
+}
+
+class CoreAPIQuoteTransformer implements CoreAPIQuote {
+  id!: string
+  content!: string
+  authorId!: string
+
+  submitterId!: string
+
+  @Type(() => Date)
+  submitDt!: Date
+
+  serverId!: string
+  channelId?: string
+  messageId?: string
+
+  @Type(() => CoreAPIQuoteReceiveTransformer)
+  receives!: CoreAPIQuoteReceiveTransformer[]
+
+  @Type(() => CoreAPIQuoteStatusDeclarationTransformer)
+  statusDeclaration?: CoreAPIQuoteStatusDeclarationTransformer
+
+  votes!: Record<string, Date>
+  requiredVoteCount!: number
+
+  @Type(() => Date)
+  expirationDt!: Date
+
+  isLegacy!: boolean
+  revision!: number
+}
+
+export function consumeAPIQuote(
+  quote: CoreAPIQuote
+): ApprovedQuote | PendingQuote {
+  const { statusDeclaration, ...others } = plainToInstance(
+    CoreAPIQuoteTransformer,
+    quote
+  )
+
+  if (!statusDeclaration) {
+    return Object.assign(others, { status: 'PENDING' }) as PendingQuote
+  } else if (statusDeclaration.status === 'APPROVED') {
+    return Object.assign(others, { status: 'APPROVED' }) as ApprovedQuote
+  } else {
+    throw new Error('Expired quotes are not yet supported.')
+  }
+}
