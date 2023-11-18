fec2d73f961c94f6b3fcea8d3e9859169068b301
Felix <jonfelixrico@gmail.com>
2023-11-19 04:15:57 +0800

    Create tests

---
+++ test/cypress/e2e-tests/login-elements.cy.ts
@@ -0,0 +1,24 @@
+import { SystemParamters } from 'src/types/system-parameters.interface'
+
+describe('login elements', () => {
+  before(() => {
+    cy.intercept('HEAD', '/api/session', {
+      statusCode: 401,
+    })
+  })
+
+  it('shows the invite url', () => {
+    cy.intercept('GET', '/api/system-parameters', {
+      body: {
+        discordBotInviteUrl: 'http://some-discord-bot-invite-url',
+      } as SystemParamters,
+    })
+
+    cy.visit('/login')
+
+    cy.dataCy('bot-invite').should('exist')
+    cy.dataCy('bot-inivite')
+      .find('a')
+      .should('have.attr', 'href', 'http://some-discord-bot-invite-url')
+  })
+})
