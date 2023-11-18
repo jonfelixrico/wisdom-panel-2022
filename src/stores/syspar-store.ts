d86ea1346422fa0255052efd0b664251efe0f16c
Felix <jonfelixrico@gmail.com>
2023-11-19 03:19:04 +0800

    Remove the API call from the syspar store

--- src/stores/syspar-store.ts
+++ src/stores/syspar-store.ts
@@ -1,28 +1,16 @@
 import { defineStore } from 'pinia'
-import { api } from 'src/boot/axios'
-import { getLogger } from 'src/boot/pino-logger'
 
 interface Store {
   syspars: null | Record<string, string>
 }
-
-const LOGGER = getLogger('store:syspars')
 
 export const useSysparStore = defineStore('syspars', {
   state: (): Store => ({
     syspars: null,
   }),
   actions: {
-    async fetchSyspars() {
-      if (this.syspars) {
-        LOGGER.debug("Skipping syspar fetch because it's already been cached")
-        return
-      }
-
-      const { data } = await api.get<Record<string, string>>(
-        '/system-parameters'
-      )
-      this.syspars = data
+    setSyspars(syspars: Record<string, string>) {
+      this.syspars = syspars
     },
   },
 })
