ec948b33c86ae73463909a34ada95ead1f0de413
Felix <jonfelixrico@gmail.com>
2023-11-19 14:17:30 +0800

    Create component test for invite button

--- test/cypress/component-tests/LoginCard.cy.ts
+++ test/cypress/component-tests/LoginCard.cy.ts
@@ -1,16 +1,34 @@
 import CLoginCard from 'components/login/CLoginCard.vue'
-import { useSysparStore } from 'src/stores/syspar-store'
 import { createTestingPinia } from '@pinia/testing'
+import { SystemParamters } from 'src/types/system-parameters.interface'
 
 describe('LoginCard', () => {
   it('shows the login button', () => {
     createTestingPinia({
       createSpy: cy.spy,
     })
-    cy.wrap(useSysparStore()).as('store')
 
     cy.mount(CLoginCard)
 
     cy.dataCy('discord-login-btn').should('exist')
+  })
+
+  it('shows the invite button', () => {
+    createTestingPinia({
+      initialState: {
+        syspars: {
+          syspars: {
+            discordBotInviteUrl: 'http://test-invite-url',
+          } as SystemParamters,
+        },
+      },
+      createSpy: cy.spy,
+    })
+
+    cy.mount(CLoginCard)
+
+    cy.dataCy('bot-invite')
+      .should('exist')
+      .should('have.attr', 'href', 'http://test-invite-url')
   })
 })
